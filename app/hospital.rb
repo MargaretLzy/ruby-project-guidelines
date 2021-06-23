b6ec8afd6b46556d07198ec49c10ddf6db9d6a24
Taek Jin Han <tj.han@yale.edu>
2021-06-23 18:21:30 -0400

    method done

--- app/hospital.rb
+++ app/hospital.rb
@@ -3,10 +3,6 @@ class Hospital < ActiveRecord::Base
     has_many :patients, through: :reviews 
 
     # average rating of a particular hospital 
-    def reviews 
-        reviews Review.all.select { |review| review.hospital_id == self.id }
-    end
-
     def average_rating
         ratings = reviews.map { |review| review.rating }
         total = ratings.reduce(0) { |sum, rating| sum + rating }
@@ -14,36 +10,37 @@ class Hospital < ActiveRecord::Base
     end
 
     # all the hospitals in your state 
-    def self.state(your_state_here)  
-        self.all.select { |hospital| hospital.state == your_state_here }
+    def self.state(your_state)  
+        self.all.select { |hospital| hospital.state == your_state }
     end
 
 
     # all the hospital in your city (if there is)
     ## return "We do not have hospital(s) registered in your city."
     ## return "Be the first one to review your hospital!"
-    def self.city(your_city_here)  
-        self.all.select { |hospital| hospital.state == your_city_here }
+    def self.city(your_city)  
+        self.all.select { |hospital| hospital.state == your_city }
     end
 
 
-    # highest rating as government hospital -> return hospital as object 
+    # 10 highest rating as government hospital -> return hospital as object 
     def self.best_government_hospitals
-        #binding.pry
-        government_hospitals = self.all.select { |hospital| hospital.hospital_type == "Government" }
-        binding.pry
-        reviews_government_hospitals = Review.all.select {|review| government_hospitals.include?(review.hospital)}
-        reviews_government_hospitals.max_by { |review| review.rating }
+        reviews_government_hospitals = Review.all.select {|review| review.hospital.hospital_type == "Government" }
+        sorted_reviews = reviews_government_hospitals.sort_by { |review| review.rating }.reverse
+        sorted_reviews[0,9]
     end
 
-    # highest ratting as private hospital -> return hospital as object 
+    # 10 highest ratting as private hospital -> return hospital as object 
     def self.best_private_hospitals
-        #binding.pry
-        private_hospitals = self.all.select { |hospital| hospital.hospital_type == "Private" }
-        binding.pry
-        reviews_private_hospitals = Review.all.select {|review| private_hospitals.include?(review.hospital)}
-        SELECT reviews_private_hospitals FROM reviews 
+        reviews_private_hospitals = Review.all.select {|review| review.hospital.hospital_type == "Private" }
+        sorted_reviews = reviews_private_hospitals.sort_by { |review| review.rating }.reverse
+        sorted_reviews[0,9]
     end
 
+    # hospital with the most reviewed 
+    ## How many reviews it had? 
+    def self.most_reviewed
+        self.all.max_by { |hospital| hospital.reviews.length }
+    end
 
 end
