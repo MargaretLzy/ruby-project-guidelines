fbcb6b63b14c7e7a39acb1b5384562ec8322309f
Taek Jin Han <tj.han@yale.edu>
2021-06-23 15:01:14 -0400

    method

--- app/hospital.rb
+++ app/hospital.rb
@@ -3,13 +3,47 @@ class Hospital < ActiveRecord::Base
     has_many :patients, through: :reviews 
 
     # average rating of a particular hospital 
+    def reviews 
+        Review.all.select { |review| review.hospital_id == self.id }
+    end
+
+    def average_rating
+        ratings = reviews.map { |review| review.rating }
+        total = ratings.reduce(0) { |sum, rating| sum + rating }
+        total / ratings.length 
+    end
 
     # all the hospitals in your state 
+    def self.state(your_state_here)  
+        self.all.select { |hospital| hospital.state == your_state_here }
+    end
+
 
     # all the hospital in your city (if there is)
+    ## return "We do not have hospital(s) registered in your city."
+    ## return "Be the first one to review your hospital!"
+    def self.city(your_city_here)  
+        self.all.select { |hospital| hospital.state == your_city_here }
+    end
+
 
     # highest ratting as government hospital -> return hospital as object 
+    def self.best_government_hospitals
+        #binding.pry
+        government_hospitals = self.all.select { |hospital| hospital.hospital_type == "Government" }
+        binding.pry
+        reviews_government_hospitals = Review.all.select {|review| government_hospitals.include?(review.hospital)}
+        reviews_government_hospitals.max_by { |review| review.rating }
+    end
 
     # highest ratting as private hospital -> return hospital as object 
+    def self.best_private_hospitals
+        #binding.pry
+        private_hospitals = self.all.select { |hospital| hospital.hospital_type == "Private" }
+        binding.pry
+        reviews_private_hospitals = Review.all.select {|review| private_hospitals.include?(review.hospital)}
+        SELECT reviews_private_hospitals FROM reviews 
+    end
+
 
 end
--- app/patient.rb
+++ app/patient.rb
@@ -3,6 +3,10 @@ class Patient < ActiveRecord::Base
     has_many :hospitals, through: :reviews 
 
     # the oldest patient
+    ## perhaps need 20 olderst people on the list 
+    def self.oldest_patient 
+        self.all.max_by { |patient| patient.age } 
+    end
 
     # the patient with the most reviews 
 
